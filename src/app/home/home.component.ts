922119ec2cce3a1b23ac494f60e671868f204f4b
EngiBeerHub <engibeer0422@gmail.com>
2024-03-03 21:32:44 +0900

    2-14: Reactive Component Interaction using Custom Observables and Behavior Subject

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Course, sortCoursesBySeqNo} from '../model/course';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {finalize, map} from 'rxjs/operators';
 import {CoursesService} from "../services/courses.service";
+import {LoadingService} from "../loading/loading.service";
 
 
 @Component({
@@ -17,7 +18,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService, private loadingService: LoadingService) {
 
   }
 
@@ -26,9 +27,11 @@ export class HomeComponent implements OnInit {
   }
 
   reloadCourses() {
+    this.loadingService.loadingOn();
     const courses$ = this.coursesService.loadAllCourses()
       .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo))
+        map(courses => courses.sort(sortCoursesBySeqNo)),
+        finalize(() => this.loadingService.loadingOff())
       );
     this.beginnerCourses$ = courses$
       .pipe(
