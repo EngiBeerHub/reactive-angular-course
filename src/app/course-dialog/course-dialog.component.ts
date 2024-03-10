670f4d3d67bbf908b26630a5993ec7825abe8e1b
EngiBeerHub <engibeer0422@gmail.com>
2024-03-10 21:02:03 +0900

    2-20: Local Error Handling in an Angular Material Dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -5,12 +5,15 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import * as moment from 'moment';
 import {CoursesService} from "../services/courses.service";
 import {LoadingService} from "../loading/loading.service";
+import {MessagesService} from "../messages/messages.service";
+import {catchError} from "rxjs/operators";
+import {throwError} from "rxjs";
 
 @Component({
   selector: 'course-dialog',
   templateUrl: './course-dialog.component.html',
   styleUrls: ['./course-dialog.component.css'],
-  providers: [LoadingService]
+  providers: [LoadingService, MessagesService]
 })
 export class CourseDialogComponent implements AfterViewInit {
 
@@ -23,7 +26,8 @@ export class CourseDialogComponent implements AfterViewInit {
     private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) course: Course,
     private coursesService: CoursesService,
-    private loadingService: LoadingService) {
+    private loadingService: LoadingService,
+    private messagesService: MessagesService) {
 
     this.course = course;
 
@@ -42,7 +46,15 @@ export class CourseDialogComponent implements AfterViewInit {
   save() {
 
     const changes = this.form.value;
-    const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes);
+    const saveCourses$ = this.coursesService.saveCourse(this.course.id, changes)
+      .pipe(
+        catchError(err => {
+          const message = "Could not save course";
+          console.log(message, err);
+          this.messagesService.showErrors(message);
+          return throwError(err);
+        })
+      );
     this.loadingService.showLoaderUntilCompleted(saveCourses$)
       .subscribe(val => {
         this.dialogRef.close(val);
